34e7cfef2bbcd8a5192b09ef21b43a812787b499
Lulu <luyizhang@users.noreply.github.com>
2025-10-04 22:32:55 -0400

    Add details

--- client/components/SponsorshipSheet.tsx
+++ client/components/SponsorshipSheet.tsx
@@ -4,11 +4,8 @@ import {
   SheetHeader,
   SheetTitle,
   SheetDescription,
-  SheetFooter,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Sponsor } from "@/lib/models/sponsors";
-import SponsorshipMetadata from "@/components/SponsorshipMetadata";
 
 interface SponsorshipSheetProps {
   videoUrl: string;
@@ -17,12 +14,27 @@ interface SponsorshipSheetProps {
   sheetChildren: React.ReactNode;
 }
 
+const enrichedDetails = [
+  {
+    label: "Detail 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    label: "Detail 2",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
 const SponsorshipSheet: React.FC<SponsorshipSheetProps> = ({
   videoUrl,
   title,
   children,
   sheetChildren,
 }) => {
+  // TODO: based on video url, extract more information
+
   return (
     <Sheet>
       <SheetTrigger>{children}</SheetTrigger>
@@ -41,7 +53,20 @@ const SponsorshipSheet: React.FC<SponsorshipSheetProps> = ({
             </span>
           </SheetDescription>
         </SheetHeader>
-        <div className="flex flex-col gap-4 px-4">{sheetChildren}</div>
+        {/* mirrors content from card */}
+        <div className="flex flex-col gap-4 px-4 text-sm">
+          {sheetChildren}
+          {/* enriched details */}
+          {enrichedDetails.map((detail, index) => (
+            <div
+              key={`detail-${index}`}
+              className="flex flex-col gap-0.5"
+            >
+              <h3>{detail.label}</h3>
+              <p className="text-sm text-slate-400">{detail.content}</p>
+            </div>
+          ))}
+        </div>
       </SheetContent>
     </Sheet>
   );
